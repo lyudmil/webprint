e2b3aea39765406214f830a1a7b8b14a7fb55b1e
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-09-26 16:10:16 +0200

    Yanked out custom validations on print jobs from patprint to make printing a more general process.

--- app/models/print_job.rb
+++ app/models/print_job.rb
@@ -3,41 +3,12 @@ class PrintJob
         @file = file
         @printer = printer
         @number_of_copies = number_of_copies
-        @validation_errors = []
     end
 
     def execute
         unless @file.blank?
             upload_file
             call_print_script
-        end
-    end
-
-    def valid?
-        validate
-        @validation_errors.empty?
-    end
-
-    def validation_errors
-        @validation_errors
-    end
-
-    private
-
-    def validate
-        validate_file
-        validate_number_of_copies
-    end
-
-    def validate_file
-        if not @file.original_filename.downcase =~ /.*\.plt/
-            @validation_errors.push "File must be a PLT."
-        end
-    end
-
-    def validate_number_of_copies
-        if not @number_of_copies.to_i <= 12
-            @validation_errors.push "Number of copies must be no more than 12."
         end
     end
 
