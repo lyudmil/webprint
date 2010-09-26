4f711ee3aea101bbd6592053169bd60fb3f8baa9
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-09-26 12:46:30 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webprint::Application.config.secret_token = '76a9046b5dfdf4df656e05acafa0e7dc68be2dc5959543e97e5ea9a62f9723ad9fba9f168001a747f46675f43fc04e8cf616312cecddee4ab84ad42b038cc013'
